3a827854c962aed3b7deb8630c166b31df0d6588
sourabhapps <sharmasourabh355@gmail.com>
2020-04-26 05:45:09 -0400

    Start

---
+++ app/src/main/java/com/sourabh/githubissuelist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sourabh.githubissuelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
