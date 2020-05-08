2c101d485f290f313dbe3f19a3e712cdbbd8036b
sourabhapps <sharmasourabh355@gmail.com>
2020-05-08 10:18:56 -0400

    fake

--- app/src/main/java/com/sourabh/githubissuelist/model/Repository.java
+++ app/src/main/java/com/sourabh/githubissuelist/model/Repository.java
@@ -33,6 +33,6 @@ public class Repository extends BaseObservable {
                 Log.e("Test", t.getMessage(), t);
             }
         };
-        Api.getApiInterface().getGitIssuesApi().enqueue(callback);
+        Api.getApi().getGitIssuesApi().enqueue(callback);
     }
 }
